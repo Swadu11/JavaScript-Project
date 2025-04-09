23d312d7e10eb93cab96d8e12e75ac31133fcdfd
Swadeshwar K <swadeshwarko@gmail.com>
2025-04-09 22:49:18 +0530

    Add files via upload

---
+++ 2.Character Counter/index.js
@@ -0,0 +1,8 @@
+const textInput = document.getElementById("textInput");
+const charCount = document.getElementById("charCount")
+
+textInput.addEventListener("input" , ()=>{
+    const remaining = 50 - textInput.value.length;
+    charCount.textContent= remaining;
+});
+
